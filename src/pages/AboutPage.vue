8663f23d3a426ec3acbde6dcc1df8da19ad9c6e6
Todd Rylaarsdam <todd@toddr.org>
2026-05-31 20:36:58 -0500

    update

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -10,7 +10,12 @@ import { site } from '../data/content'
     </div>
 
     <div class="content-panel markdown-body">
-     
+     I'm a software engineer and audio engineer based in Seattle. Currently, I'm a software engineer at Microsoft, where I work with the Excel team. In my free time, I'm a freelance audio engineer, theatrical sound designer, and sound system engineer. I hold a BS in Computer Science from George Fox University (2026)
+      <br/><br/>
+     In the past, I was the lead sound technician for George Fox University's Theatre, where I also worked as the A1 and sound designer for many productions over the course of my 4 years there. I also have worked for Broadway Rose Theatre Company, and a number of other small theatres in the Portland area. I'm also a
+     viola player in orchestras and pit orchestras, and have served as the principal viola in the George Fox University Symphony Orchestra for 4 years.
+      <br/><br/>
+     My passion is combining my engineering skills to create reliable and high-quality sound systems to reinforce and support live performance. If you're looking for a sound designer, A1, or system designer, feel free to reach out.
     </div>
   </section>
 </template>
